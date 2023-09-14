7aab5568eda9f3b085f14881842a1b58711c6d54
Thiago Chiqueti <104872408+ThiagoChiqueti@users.noreply.github.com>
2023-09-14 13:58:04 -0300

    ajustes do admin

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 
 import { sequelize } from './database'
+import { adminJS, adminJSRouter } from './adminjs'
 
 const app = express()
+
+app.use(express.static('public'))
+
+//app.use(caminho, rotas)
+app.use(adminJS.options.rootPath, adminJSRouter)
 
 const PORT = process.env.PORT || 3000
 

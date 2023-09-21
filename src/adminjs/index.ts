d7ac920a3ccbfe783bc031d789cee450a237e2ee
Thiago Chiqueti <104872408+ThiagoChiqueti@users.noreply.github.com>
2023-09-21 19:39:16 -0300

    add translate

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
 import { User } from "../models";
+import { locale } from "./locale";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -12,6 +13,7 @@ export const adminJS = new AdminJS({
   databases: [sequelize],
   rootPath: "/admin",
   resources: adminJsResources,
+  locale: locale,
   branding: {
     companyName: "OneBitFlix",
     logo: "/logoOnebitflix.svg",
